f9ab922f6b91fd0b38b691c3cb37236682625e07
pewcia <ewelina.serafin996@gmail.com>
2025-08-05 15:36:40 +0200

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Form() {
+  let [city, setCity] = useState("");
+  let [forecast, setForecast] = useState(null);
+
+  function cityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setForecast(null);
+
+    let apiKey = "bb203f4a92e7t89f33c44201bd2fbfoa";
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(url).then(showForecast);
+    console.log(url);
+  }
+
+  function showForecast(response) {
+    console.log(response);
+    setForecast({
+      temperature: response.data.temperature,
+      condition: response.data.condition,
+      wind: response.data.wind,
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city.."
+          value={city}
+          onChange={cityChange}
+        />
+        <input type="submit" value="Search" />
+      </form>
+
+      {forecast && (
+        <div>
+          <h3>Weather forecast:</h3>
+          <p> Temperature: {Math.round(forecast.temperature.current)}°C </p>
+          <p> Description: {forecast.condition.description} </p>
+          <p> Humidity: {forecast.temperature.humidity} % </p>
+          <p> Wind: {Math.round(forecast.wind.speed)} km/h </p>
+          <img src={forecast.condition.icon_url} />
+        </div>
+      )}
+    </div>
+  );
+}
